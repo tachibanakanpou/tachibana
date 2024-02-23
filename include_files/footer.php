da39f4cde74b0219d4bfac45ab396fc6fedaee3c
tachibanakanpou <101479142+tachibanakanpou@users.noreply.github.com>
2024-02-23 10:32:17 +0900

    Add files via upload

---
+++ include_files/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <div id="copyright">
+    <small>Copyright&copy2022 <a href="index.php">立花漢方内科小児科</a> All Rights Reserved.</small>  
+  </div>
+</footer>
---
+++ include_files/header.php
@@ -0,0 +1,56 @@
+<header class="header">
+  <h1 id="logo"><img src="img/logo.png"  alt="立花漢方内科小児科"></h1>
+  <div class="addressham">
+    <div>
+      <address><span class="tel">097-578-7078</span><br>
+        受付時間：AM8:30〜12:00 PM14:00〜17:00</address>
+    </div>
+    <div id="btn">
+      <span></span>
+      <span></span>
+      <span></span>
+      <samp class="hamburger__text">MENU</samp>
+    </div>
+  </div>
+  <div>
+    <nav id="menu">
+      <li class="menu-item">
+        <a href="index.php">ホーム</a>
+      </li>
+      <li class="menu-item drop-menu">
+        <a href="#" ">概　要</a>
+        <ul class="drop-menu-list">
+          <li class="drop-menu-item">
+            <a href="ta__gai.php">診療時間</a>
+          </li>
+          <li class="drop-menu-item">
+            <a href="ta__sha.php">院内写真</a>
+          </li>
+          <li class="drop-menu-item">
+            <a href="ta__acc.php">アクセス</a>
+          </li>
+          <li class="drop-menu-item">
+            <a href="ta__chu.php">駐車場</a>
+          </li>
+        </ul>
+      </li> 
+      <li class="menu-item drop-menu">
+        <a href="#" >ご挨拶</a>
+        <ul class="drop-menu-list">
+          <li class="drop-menu-item">
+            <a href="ta__ai1.php">ご挨拶１</a>
+          </li>
+          <li class="drop-menu-item">
+            <a href="ta__ai2.php">ご挨拶２</a>
+          </li>
+          <li class="drop-menu-item">
+            <a href="ta__kan.php">院長閑話</a>
+          </li>
+        </ul>
+      </li> 
+      <li class="menu-item drop-menu">
+        <a href="ta__osi.php">お知らせ</a>
+      </li> 
+    </nav>
+  </div>
+</header>
