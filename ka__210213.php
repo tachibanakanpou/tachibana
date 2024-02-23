e34bf1bbdacb166fc0308fa2bb267044211be24e
tachibanakanpou <101479142+tachibanakanpou@users.noreply.github.com>
2024-02-23 10:29:18 +0900

    Add files via upload

---
+++ ka__210213.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>立花漢方内科小児科</title> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="立花漢方内科小児科">
+    <meta name="description" content=" 漢方治療を中心に行っているクリニック">
+    <meta name="keywords" content="漢方">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!-- header -->
+    <?php include($_SERVER['DOCUMENT_ROOT']."/include_files/header.php"); ?>
+    <!-- header -->
+
+    <main> 
+      <!--2021 02 13-->
+      <div class="kanwa20210213">
+        <div class="box2">
+          <img src="imgos/hon.jpg" alt="本">
+        </div> 
+        <div class="box1">
+          <h2>新型コロナウイルス感染症</h2>
+          <h3>ー　私ならば漢方薬を基本に総合力で対処　ー</h3>
+            <p>漢方医学的には「ただ寝んと欲す」、 即ち脈が微細で、 ただ寝ていたいという少陰病の症状です。 <br>
+            新型コロナウイルス感染症は病初期から「怠さ」が出ることが多いようです。<br>
+            普通の上気道感染症ならば初期は麻黄湯などの太陽病から始まりますが、 今回はいきなり寒さ・怠さの強い少陰病、厥陰病に陥っています。</p>
+            <p>
+              私の著書に記載していますが（81頁）、 このよう場合は少陰病、 厥陰病の漢方薬である真武湯や人参湯を服用して、 まず元気をつけることが第一です。<br>
+              それと同時に吸収されやすいイオン化したミネラルが必須です。<BR>
+              さらには免疫の要である制御性Ｔ細胞を活性化する酪酸が必要です。<BR>
+              そして体温以下の飲み物、 食べ物は摂らない、 動物性蛋白をしっかり摂取する、 8時間以上の睡眠をとることです。
+            </p>  
+        </div>
+      </div>     
+      <!-- footer -->
+      <?php include($_SERVER['DOCUMENT_ROOT']."/include_files/footer.php"); ?>
+      <!-- footer -->
+    </main>
+    <script src="ham.js"></script>
+  </body>
+</html>
---
+++ os__220707.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>立花漢方内科小児科</title> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="立花漢方内科小児科">
+    <meta name="description" content=" 漢方治療を中心に行っているクリニック">
+    <meta name="keywords" content="漢方">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!-- header -->
+    <?php include($_SERVER['DOCUMENT_ROOT']."/include_files/header.php"); ?>
+    <!-- header -->
+
+    <main> 
+      <!--お知らせ-->
+      <section class="os10">
+        <table class="os20">
+          <caption class="bgcolor2">お知らせ</caption> 
+          <tr>
+            <!-- <th>2022/07/07</th> -->
+            <td>
+              <img src="imgos/20220707.jpg"alt="発熱外来のお知らせ">
+            </td>         
+          </tr>
+        </table>
+      </section>
+      
+      <!-- footer -->
+      <?php include($_SERVER['DOCUMENT_ROOT']."/include_files/footer.php"); ?>
+      <!-- footer -->
+    </main>
+    <script src="ham.js"></script>
+  </body>
+</html>
