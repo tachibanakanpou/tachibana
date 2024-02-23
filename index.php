54ac541aedecc3611882f14b1cbcdb0679dd4e59
tachibanakanpou <101479142+tachibanakanpou@users.noreply.github.com>
2024-02-23 10:28:26 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>立花漢方内科小児科</title> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="立花漢方内科小児科">
+    <meta name="description" content=" 漢方治療を中心に行っているクリニック">
+    <meta name="keywords" content="漢方">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!-- header -->
+    <?php include($_SERVER['DOCUMENT_ROOT']."/include_files/header.php"); ?>
+    <!-- header -->
+    <main> 
+      <article class="face">
+        <!--メッセージ-->
+        <div class="exp1">
+           <p>赤ちゃんから高齢者まで幅広く受診でき、</p>
+           <p>漢方治療はもちろん、てんかん・難病治療も行っています。</p>
+           <p>また、疲れやすい、食欲不振、風邪を引きやすい、</p>
+           <p>腹痛・便秘・下痢、冷え症、頭痛、鼻炎、月経痛、ストレス性障害、</p>
+           <p>湿疹(皮膚炎)、更年期等の症状も漢方薬で改善が出来ます。</p>
+        </div>
+       
+       
+        <!--Facebook-->
+        <div class="exp2">
+          <p >最新情報は<br><a href="https://www.facebook.com/tatibanakanpounaikasiyounika/" target="_blank">Facebook</a>にてお知らせ致します。</p>
+        </div>
+      </article>      
+      <!--slideshow----------------------------------->
+      <div class="slideBox">
+        <img class="item1" src="img/gai600.jpg" alt="">
+        <img class="item1" src="img/matiai3.png" alt="">
+        <img class="item1" src="img/sinsatu1.png" alt="">
+      </div>   
+      <!-- footer -->
+      <?php include($_SERVER['DOCUMENT_ROOT']."/include_files/footer.php"); ?>
+      <!-- footer -->
+    </main>
+    <script src="ham.js"></script>
+  </body>
+</html>
